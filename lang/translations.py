33772d734eed6fda2933178c634fd9348f506541
12193696 <62126566+Argoneon1810@users.noreply.github.com>
2022-12-18 04:17:13 +0900

    Multi-language support

---
+++ lang/translations.py
@@ -0,0 +1,43 @@
+key_application_title = "title"
+key_debug_no_settings_saved = "debug_no_settings_saved"
+key_no_such_file_exists = "debug_no_such_file_exists"
+key_apply_rotation_to = "menu_apply_rotation_to"
+key_display_index = "menu_display_index"
+key_screen_orientation = "menu_screen_orientation"
+key_landscape = "landscape"
+key_portrait = "portrait"
+key_landscape_flipped = "landscape_flipped"
+key_portrait_flipped = "portrait_flipped"
+key_translations = "translations"
+key_exit = "exit"
+
+translations = {
+    "en_US": {
+        key_application_title: "Screen Rotation Control",
+        key_debug_no_settings_saved: "no settings saved",
+        key_no_such_file_exists: "no such file exists",
+        key_apply_rotation_to: "Apply rotation to:",
+        key_display_index: "Display #%d",
+        key_screen_orientation: "Screen orientation:",
+        key_landscape: "Landscape",
+        key_portrait: "Portrait",
+        key_landscape_flipped: "Landscape_Flipped",
+        key_portrait_flipped: "Portrait_Flipped",
+        key_translations: "Translations:",
+        key_exit: "Exit",
+    },
+    "ko_KR": {
+        key_application_title: "화면 회전 조작",
+        key_debug_no_settings_saved: "저장된 설정 없음",
+        key_no_such_file_exists: "파일을 찾을 수 없음",
+        key_apply_rotation_to: "화면 회전 적용 대상:",
+        key_display_index: "디스플레이 #%d",
+        key_screen_orientation: "화면 방향:",
+        key_landscape: "가로",
+        key_portrait: "세로",
+        key_landscape_flipped: "가로 (대칭 이동)",
+        key_portrait_flipped: "세로 (대칭 이동)",
+        key_translations: "번역:",
+        key_exit: "종료",
+    }
+}
